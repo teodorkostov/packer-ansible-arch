cc3f7a863b9e74974d7963c2930e34143d932035
Teodor Kostov <kostov.teodor@gmail.com>
2017-06-24 18:00:50 +0200

    Fixed the locale.

--- resources/initial.sh
+++ resources/initial.sh
@@ -31,4 +31,4 @@ pacman --noconfirm -S ansible
 # install the base system
 pacstrap /mnt base base-devel python sudo bash-completion mesa gnome networkmanager xf86-video-amdgpu xf86-video-fbdev
 genfstab -U -p /mnt >> /mnt/etc/fstab
-bootctl --path=/mnt/boot install || true
+bootctl --path=/mnt/boot install || true # modifies the OVMF image
