730affd182a5f79a092bf1fec5afb9648bcdd3a2
Teodor Kostov <teodor.kostov@ing.com>
2019-03-14 16:59:47 +0100

    Parametrized the device in the initial.sh

--- resources/initial.sh
+++ resources/initial.sh
@@ -2,26 +2,28 @@
 
 set -e
 
+DEVICE=/dev/sda
+
 # preparing the hdd
-parted -m -s /dev/vda mklabel gpt
-parted -m -s /dev/vda mkpart ESP fat32 1MiB 513MiB
-parted -m -s /dev/vda set 1 boot on
-parted -m -s /dev/vda mkpart primary linux-swap 513MiB 16GiB
-parted -m -s /dev/vda mkpart primary ext4 16GiB 100%
-parted -m -s /dev/vda name 3 'os'
+parted -m -s "${DEVICE}" mklabel gpt
+parted -m -s "${DEVICE}" mkpart ESP fat32 1MiB 513MiB
+parted -m -s "${DEVICE}" set 1 boot on
+parted -m -s "${DEVICE}" mkpart primary linux-swap 513MiB 16GiB
+parted -m -s "${DEVICE}" mkpart primary ext4 16GiB 100%
+parted -m -s "${DEVICE}" name 3 'os'
 sleep 1
 
-mkfs.fat -F32 /dev/vda1
-mkswap /dev/vda2
-swapon /dev/vda2
-mkfs.ext4 /dev/vda3
+mkfs.fat -F32 "${DEVICE}1"
+mkswap "${DEVICE}2"
+swapon "${DEVICE}2"
+mkfs.ext4 "${DEVICE}3"
 
-parted -s /dev/vda print
+parted -s "${DEVICE}" print
 
 # mounting the folders
-mount /dev/vda3 /mnt
+mount "${DEVICE}3" /mnt
 mkdir /mnt/boot
-mount /dev/vda1 /mnt/boot
+mount "${DEVICE}1" /mnt/boot
 mkdir -p /mnt/boot/loader/entries
 
 # prepare ansible
